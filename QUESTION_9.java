50a2add989b5751246c63fe6a61b32ccc2f754ac
subhasamikshya01 <165682929+subhasamikshya01@users.noreply.github.com>
2024-04-18 19:22:59 +0530

    Update QUESTION_9.java

--- QUESTION_9.java
+++ QUESTION_9.java
@@ -1,118 +1,147 @@
-package QUESTION_9;
-
+import java.util.ArrayList;
 import java.util.List;
 
-interface EnrollmentSystem{
-	void enrollStudent(Student s,Course c);
-	void dropStudent(Student s,Course c);
-	void displayEnrollmentDetails();
+// Interface representing the enrollment system
+interface EnrollmentSystem {
+    void enrollStudent(Student student, Course course);
+    void dropStudent(Student student, Course course);
+    void displayEnrollmentDetails();
 }
-class Student{
-	public Student(String n, String sid) {
-		super();
-		this.n = n;
-		this.sid = sid;
-	}
 
-	public String getN() {
-		return n;
-	}
+// Student class
+class Student {
+    private String studentId;
+    private String name;
 
-	public void setN(String n) {
-		this.n = n;
-	}
+    public Student(String studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
 
-	public String getSid() {
-		return sid;
-	}
+    // Getters and setters
+    public String getStudentId() {
+        return studentId;
+    }
 
-	public void setSid(String sid) {
-		this.sid = sid;
-	}
-
-	private String n,sid;
-	
+    public String getName() {
+        return name;
+    }
 }
-class Course{
-	public Course(String cid, String cn) {
-		super();
-		this.cid = cid;
-		this.cn = cn;
-	}
 
-	public String getCid() {
-		return cid;
-	}
+// Course class
+class Course {
+    private String courseId;
+    private String courseName;
+    private List<Student> enrolledStudents;
 
-	public void setCid(String cid) {
-		this.cid = cid;
-	}
+    public Course(String courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.enrolledStudents = new ArrayList<>();
+    }
 
-	public String getCn() {
-		return cn;
-	}
+    // Getters
+    public String getCourseId() {
+        return courseId;
+    }
 
-	public void setCn(String cn) {
-		this.cn = cn;
-	}
+    public String getCourseName() {
+        return courseName;
+    }
 
-	private String cid,cn;
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    // Method to enroll a student
+    public void enrollStudent(Student student) {
+        enrolledStudents.add(student);
+    }
+
+    // Method to drop a student
+    public void dropStudent(Student student) {
+        enrolledStudents.remove(student);
+    }
 }
-class Enrollment implements EnrollmentSystem{
-	public Enrollment() {
-		super();
-		this.es = es;
-		this.ec = ec;
-	}
 
-	public List<Student> getEs() {
-		return es;
-	}
+// Enrollment class implementing EnrollmentSystem interface
+class Enrollment implements EnrollmentSystem {
+    private List<Student> students;
+    private List<Course> courses;
 
-	public void setEs(List<Student> es) {
-		this.es = es;
-	}
+    public Enrollment() {
+        this.students = new ArrayList<>();
+        this.courses = new ArrayList<>();
+    }
 
-	public List<Course> getEc() {
-		return ec;
-	}
+    // Method to enroll a student in a course
+    @Override
+    public void enrollStudent(Student student, Course course) {
+        if (!students.contains(student)) {
+            students.add(student);
+        }
+        course.enrollStudent(student);
+    }
 
-	public void setEc(List<Course> ec) {
-		this.ec = ec;
-	}
+    // Method to drop a student from a course
+    @Override
+    public void dropStudent(Student student, Course course) {
+        if (students.contains(student)) {
+            course.dropStudent(student);
+        }
+    }
 
-	private List<Student> es;
-	private List<Course> ec;
-	@Override
-	public void enrollStudent(Student s, Course c) {
-		es.add(s);
-		ec.add(c);
-		System.out.println("NAME:- "+s.getN()+" COURSE:- "+c.getCn());
-	}
+    // Method to display enrollment details
+    @Override
+    public void displayEnrollmentDetails() {
+        for (Course course : courses) {
+            System.out.println("Course: " + course.getCourseName());
+            System.out.println("Enrolled Students:");
+            for (Student student : course.getEnrolledStudents()) {
+                System.out.println("Student ID: " + student.getStudentId() + ", Name: " + student.getName());
+            }
+            System.out.println();
+        }
+    }
 
-	@Override
-	public void dropStudent(Student s, Course c) {
-		
-	}
-
-	@Override
-	public void displayEnrollmentDetails() {
-		
-	}
-	
+    // Method to add a course
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
 }
-public class QUESTION_9 {
 
-	public static void main(String[] args) {
-		Enrollment e=new Enrollment();
-		Student s1=new Student("S1","N1");
-		Student s2=new Student("S2","N2");
-		Student s3=new Student("S3","N3");
-		Student s4=new Student("S4","N4");
-		Course c1=new Course("CN1","CID1");
-		Course c2=new Course("CN2","CID2");
-		Course c3=new Course("CN3","CID3");
-		Course c4=new Course("CN4","CID4");
-	}
+// Main class to demonstrate the functionality
+public class Main {
+    public static void main(String[] args) {
+        // Creating enrollment system
+        EnrollmentSystem enrollmentSystem = new Enrollment();
 
+        // Creating students
+        Student student1 = new Student("101", "Alice");
+        Student student2 = new Student("102", "Bob");
+
+        // Creating courses
+        Course course1 = new Course("C101", "Java Programming");
+        Course course2 = new Course("C102", "Data Structures");
+
+        // Adding courses to the enrollment system
+        ((Enrollment) enrollmentSystem).addCourse(course1);
+        ((Enrollment) enrollmentSystem).addCourse(course2);
+
+        // Enrolling students in courses
+        enrollmentSystem.enrollStudent(student1, course1);
+        enrollmentSystem.enrollStudent(student2, course1);
+        enrollmentSystem.enrollStudent(student2, course2);
+
+        // Displaying enrollment details
+        System.out.println("Enrollment Details:");
+        enrollmentSystem.displayEnrollmentDetails();
+
+        // Dropping a student from a course
+        enrollmentSystem.dropStudent(student2, course1);
+
+        // Displaying updated enrollment details
+        System.out.println("Updated Enrollment Details:");
+        enrollmentSystem.displayEnrollmentDetails();
+    }
 }
